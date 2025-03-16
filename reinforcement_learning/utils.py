63fbadb785b832e5651d34040cb3b3a8622b17b9
Jordan Huynh <jordanhuynh784@gmail.com>
2025-03-15 20:47:52 -0700

    Added Frame Function

--- reinforcement_learning/utils.py
+++ reinforcement_learning/utils.py
@@ -1,6 +1,15 @@
 import torch
+import pygame
 
 select_device = "cpu"
 
 if select_device in ["mps", "cuda", "cpu"]: device = torch.device(select_device)
 else: device = torch.device("mps" if torch.backends.mps.is_available() else "cuda" if torch.cuda.is_available() else "cpu")
+
+def get_last_frames(surface, width, height):
+    #For now, 1 frame
+    pygame.image.save(surface, "./frames/last_frame.png")
+    image = pygame.image.load("./frames/last_frame.png")
+
+    downscale = pygame.transform.scale(image, (width, height))
+    pygame.image.save(downscale, "./frames/last_frame_downscale.png")
