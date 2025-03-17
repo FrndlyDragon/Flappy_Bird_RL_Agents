9fe01374090bf4c707e23947346f432c73abfca1
Jordan Huynh <jordanhuynh784@gmail.com>
2025-03-17 00:42:43 -0700

    Things

--- reinforcement_learning/cnn.py
+++ reinforcement_learning/cnn.py
@@ -3,26 +3,32 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 from torch.distributions import Categorical
+import flappy_bird_env # noqa
+import gymnasium as gym
 
 from reinforcement_learning.base_model import BaseAgents
 from reinforcement_learning.utils import device
 from game.sprites import Bird
 from game.utils import window_width, window_height
 
-class ConvolutionModel(nn.modules):
+#env = gym.make("FlappyBird-v0")
+
+class ConvolutionModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.network = nn.Sequential(
-            nn.Conv2d(4, 32, 8, stride=4),
-            nn.ReLU(),
-            nn.Conv2d(32, 64, 4, stride=2),
-            nn.ReLU(),
-            nn.Conv2d(64, 64, 3, stride=1),
-            nn.ReLU(),
-            nn.Flatten(),
-            nn.Linear(3136, 512),
-            nn.ReLU(),
-            nn.Linear(512, 1),
-        )
+        self.conv1 = nn.Conv2d(4, 32, 8, stride=4)
+        self.conv2 = nn.Conv2d(32, 64, 4, stride=2)
+        self.conv3 = nn.Conv2d(64, 64, 3, stride=1)
+
+        self.fc1 = nn.Linear(3136, 512)
+        self.fc2 = nn.Linear(512, 2)
+
     def forward(self, x):
-        return self.network(x)
+        x = nn.ReLU(self.conv1(x))
+        x = nn.ReLU(self.conv2(x))
+        x = nn.ReLU(self.conv3(x))
+
+        x = x.view(x.size(0), -1)
+
+        x = nn.ReLU(self.fc1(x))
+        return self.fc2(x)
