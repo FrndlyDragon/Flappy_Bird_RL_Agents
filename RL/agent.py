a9d556aed55c3cdb79685f509047bbcdf0c1db1a
Ewen Dano <ewenstvallier@gmail.com>
2025-03-17 17:48:43 -0700

    implemented epsilon exploration

--- RL/agent.py
+++ RL/agent.py
@@ -1,27 +1,37 @@
 from RL.policyNetwork import *
 import torch.optim as optim
 import torch
+import numpy as np
 
 class REINFORCE: 
-    def __init__(self, network='baseline', lr=0.01, gamma=0.99) -> None:
+    def __init__(self, network='baseline', lr=0.01, gamma=0.99, epsilon_exploration = False, epsilon_start=1.0, epsilon_end =0.001, epsilon_decay = 0.9) -> None:
         match network:
             case 'baseline': self.policy = Baseline()
             case 'CNN': self.policy = CNN()
         self.optimizer = optim.Adam(self.policy.parameters(), lr=lr)
+        self.epsilon_exploration = epsilon_exploration
         self.gamma = gamma
+        self.epsilon = epsilon_start
+        self.epsilon_end = epsilon_end
+        self.epsilon_decay = epsilon_decay
         self.log_probs = []
         self.rewards = []
     
     def input_type(self):
         return self.policy.input_type()
     
-    def select_action(self, state):
+    def select_action(self, state, training= True):
         state = torch.tensor(state, dtype=torch.float32)
         action_probs = self.policy(state)
         action_dist = torch.distributions.Categorical(action_probs)
-        action = action_dist.sample()
-        self.log_probs.append(action_dist.log_prob(action))  # Store log probability
-        return action.item()
+        if self.epsilon_exploration and training and np.random.random() < self.epsilon:
+            action = np.random.choice(range(action_probs.shape[0]))
+            log_prob = action_dist.log_prob(torch.tensor(action))
+        else:
+            action = action_dist.sample().item()
+            log_prob = action_dist.log_prob(torch.tensor(action))
+        self.log_probs.append(log_prob)
+        return action
     
     def store_reward(self, reward):
         self.rewards.append(reward)
@@ -44,3 +54,5 @@ class REINFORCE:
         self.optimizer.step()
         self.log_probs = []
         self.rewards = []
+
+        self.epsilon = max(self.epsilon_end, self.epsilon * self.epsilon_decay)
