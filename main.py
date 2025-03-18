1ffa80f40e9b5d31a7b3d88d9361b36868b7bc78
Jordan Huynh <jordanhuynh784@gmail.com>
2025-03-18 13:30:13 -0700

    Update main.py

--- main.py
+++ main.py
@@ -18,11 +18,8 @@ baseline:
 - epsilon_decay: 0.9
 """
 
-<<<<<<< HEAD
-network = 'customCNN'  # "baseline" or "customCNN" or "pretrainedCNN" or FF
-=======
 network = 'customCNN_MF'  # "baseline" or "customCNN" or "customCNN_MF" or "pretrainedCNN" or FF
->>>>>>> cab3a20793287758d88cfd6b824ed083139d0cbe
+
 mode = "deepq"  # "policy_grad" or "deepq"
 
 if __name__ == "__main__":
@@ -33,20 +30,12 @@ if __name__ == "__main__":
     elif mode == "policy_grad": model = REINFORCE
     else: raise ValueError(f'{mode} not implemented, only "policy_grad" or "deepq"')
 
-<<<<<<< HEAD
-    agent = model(network=network, lr=5e-3, batch_size=128, target_update_freq=150, epsilon_decay=0.99, epsilon_exploration=False)
-
-    pretrain(agent, epochs=30, dataset_size=5000, batch_size=64, lr=2e-3,
-             save_dataset=True, use_saved=True, dataset_path="data/pretrained_dataset_signle_frame_84.pth", nframes=1)
-    #agent.policy.freeze_pretrain()
-=======
     agent = model(network=network, lr=5e-3, batch_size=64, target_update_freq=200, epsilon_decay=0.95, epsilon_exploration=True)
     DynamicRules().set_rules(275, 5, 25)
 
     if network != 'baseline':
         pretrain(agent, epochs=50, dataset_size=5000, batch_size=64, lr=2e-3,
                 save_dataset=True, use_saved=False, dataset_path="pretrained_dataset_4_frames.pth", nframes=4)
->>>>>>> cab3a20793287758d88cfd6b824ed083139d0cbe
 
     # train
     policy, mean_scores, rulechange_epochs = train(agent, 2000)
